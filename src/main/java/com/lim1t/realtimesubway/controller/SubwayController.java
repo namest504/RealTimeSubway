359239cdedd981ae75e3bb0f27cf040f607631d2
List <61047602+namest504@users.noreply.github.com>
2023-07-10 08:49:39 +0900

    feat: 실시간 지하철 역 기준 상행선&하행선 상태 데이터

--- src/main/java/com/lim1t/realtimesubway/controller/SubwayController.java
+++ src/main/java/com/lim1t/realtimesubway/controller/SubwayController.java
@@ -1,11 +1,13 @@
 package com.lim1t.realtimesubway.controller;
 
+import com.lim1t.realtimesubway.dto.SubwayInfoDto.StationInfo;
 import com.lim1t.realtimesubway.dto.SubwayInfoDto.SubwayInfoResponse;
 import com.lim1t.realtimesubway.service.SubwayServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -14,13 +16,26 @@ public class SubwayController {
 
     private final SubwayServiceImpl subwayServiceImpl;
 
+
+    /**
+     * 실시간 지하철 역 기준 상행선&하행선 상태 데이터
+     *
+     * @param subwayNumber
+     * @return Flux<StationInfo>
+     */
+    @GetMapping("/station")
+    public Flux<StationInfo> parsingStationInfo(@RequestParam int subwayNumber) {
+        return subwayServiceImpl.parsingIdWithName(subwayNumber);
+    }
+
     /**
      * API 정보 요청
-     * @example 1호선 정보 요청 : http://localhost:8080/rawjson?startIdx=0&endIdx=99&subwayNumber=1
+     *
      * @param startIdx
      * @param endIdx
      * @param subwayNumber
      * @return Mono<SubwayInfoResponse>
+     * @example 1호선 정보 요청 : http://localhost:8080/rawjson?startIdx=0&endIdx=99&subwayNumber=1
      */
     @GetMapping("/rawjson")
     public Mono<SubwayInfoResponse> requestInfo(
