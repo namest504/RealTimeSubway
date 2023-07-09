1a046f00b69f18f172169ab8776f4ac8d68bcd9d
List <61047602+namest504@users.noreply.github.com>
2023-07-09 22:11:32 +0900

    init

---
+++ src/main/java/com/lim1t/realtimesubway/RealTimeSubwayApplication.java
@@ -0,0 +1,13 @@
+package com.lim1t.realtimesubway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeSubwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeSubwayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lim1t/realtimesubway/RealTimeSubwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lim1t.realtimesubway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeSubwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
