87fd7048a4b1c4119d5fe682c8a52548f6285797
List <61047602+namest504@users.noreply.github.com>
2023-07-10 17:39:43 +0900

    feat: 반환값 추가

--- src/main/java/com/lim1t/realtimesubway/service/SubwayServiceImpl.java
+++ src/main/java/com/lim1t/realtimesubway/service/SubwayServiceImpl.java
@@ -33,11 +33,16 @@ public class SubwayServiceImpl implements SubwayService {
                     List<StationInfo> stationInfoList = new ArrayList<>();
 
                     for (SubwayInfoDto.RealtimePosition position : realtimePositionList) {
-                        int stationId = Integer.parseInt(position.getStatnId());
-                        String stationName = position.getStatnNm();
-                        String stationLine = position.getUpdnLine();
-                        String stationStatus = position.getTrainSttus();
-                        stationInfoList.add(new StationInfo(stationId, stationName, stationLine, stationStatus));
+                        stationInfoList.add(new StationInfo(
+                                position.getStatnId(),
+                                position.getStatnNm(),
+                                position.getTrainNo(),
+                                position.getUpdnLine(),
+                                position.getTrainSttus(),
+                                position.getStatnTid(),
+                                position.getStatnTnm(),
+                                position.getDirectAt(),
+                                position.getLstcarAt()));
                     }
                 /*
                 todo: 중복 제거 및 정렬 필요한지 실제 데이터 비교후 확인 필요
