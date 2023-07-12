379cfa2e197779d19a6173601e6a0ae60c0d959a
List <61047602+namest504@users.noreply.github.com>
2023-07-12 21:22:35 +0900

    feat: Arrival 정보 rawjson

--- src/main/java/com/lim1t/realtimesubway/controller/SubwayController.java
+++ src/main/java/com/lim1t/realtimesubway/controller/SubwayController.java
@@ -1,11 +1,10 @@
 package com.lim1t.realtimesubway.controller;
 
+import com.lim1t.realtimesubway.dto.StationInfoDto.StationInfoResponse;
 import com.lim1t.realtimesubway.dto.SubwayInfoDto.StationInfo;
 import com.lim1t.realtimesubway.dto.SubwayInfoDto.SubwayInfoResponse;
 import com.lim1t.realtimesubway.service.SubwayServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +18,16 @@ public class SubwayController {
 
     private final SubwayServiceImpl subwayServiceImpl;
 
-
+    /**
+     * 실시간 역 기준 지하철 도착 정보 요청
+     * @param subwayName
+     * @return
+     */
+    @GetMapping("/rawjson/arrival")
+    public Mono<ResponseEntity<Mono<StationInfoResponse>>> getArrivalInfo(@RequestParam String subwayName) {
+        Mono<StationInfoResponse> stationArrivalInfo = subwayServiceImpl.findStationArrivalInfo(subwayName);
+        return Mono.just(ResponseEntity.ok().body(stationArrivalInfo));
+    }
     /**
      * 실시간 지하철 기준 상행선&하행선 상태 데이터
      *
@@ -27,10 +35,10 @@ public class SubwayController {
      * @return Flux<StationInfo>
      */
     @GetMapping("/station")
-    public ResponseEntity<Flux<StationInfo>> parsingStationInfo(@RequestParam int subwayNumber) {
+    public Mono<ResponseEntity<Flux<StationInfo>>> parsingStationInfo(@RequestParam int subwayNumber) {
 
         Flux<StationInfo> trainInfoByStation = subwayServiceImpl.getTrainInfoByStation(subwayNumber);
-        return ResponseEntity.ok().body(trainInfoByStation);
+        return Mono.just(ResponseEntity.ok().body(trainInfoByStation));
     }
 
     /**
@@ -42,13 +50,13 @@ public class SubwayController {
      * @return Mono<SubwayInfoResponse>
      * @example 1호선 정보 요청 : http://localhost:8080/rawjson?startIdx=0&endIdx=99&subwayNumber=1
      */
-    @GetMapping("/rawjson")
-    public ResponseEntity<Mono<SubwayInfoResponse>> requestInfo(
+    @GetMapping("/rawjson/staion")
+    public Mono<ResponseEntity<Mono<SubwayInfoResponse>>> requestInfo(
             @RequestParam int startIdx,
             @RequestParam int endIdx,
             @RequestParam int subwayNumber
     ) {
         Mono<SubwayInfoResponse> allInfo = subwayServiceImpl.findAllInfo(startIdx, endIdx, subwayNumber);
-        return ResponseEntity.ok().body(allInfo);
+        return Mono.just(ResponseEntity.ok().body(allInfo));
     }
 }
